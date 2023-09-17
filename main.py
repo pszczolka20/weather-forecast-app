24e5171612e3711ad151e59af08c142cb9ad09b9
pszczolka <aleksandra.pasieka32@gmail.com>
2023-09-17 18:40:51 +0200

    Day 33: Manage errors and add description to the images

--- main.py
+++ main.py
@@ -2,16 +2,33 @@ import streamlit as st
 import plotly.express as px
 from backend import get_data
 
+# add title, text input, slider, selectbox, subheader
 st.title("Weather Forecast for the Next Days")
+
 place = st.text_input("Place: ")
+
 days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help="Select the number of forecasted days")
 option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-data = get_data(place, days, option)
+if place:
+    try:
+        # Get the temperature/sky condition
+        filtered_data = get_data(place, days)
+        dates = [dict["dt_txt"] for dict in filtered_data]
 
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] / 10 for dict in filtered_data]
+            # Create a temperature plot
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
 
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+        if option == "Sky":
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            images = [f"images/{condition.lower()}.png" for condition in sky_conditions]
+            st.image(images, dates, width=115)
+
+    except KeyError:
+        st.warning("Please enter a valid city", icon="⚠️")
